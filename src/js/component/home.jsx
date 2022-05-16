4790f8381b89362e28590bf936140c211e408b79
kaiserone01 <mrporteles@gmail.com>
2022-05-16 17:26:51 +0000

    entrega ejercicio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,48 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import { Tarea } from "./tarea.jsx";
+const API_URL = "https://assets.breatheco.de/apis/fake/todos/user/usuarioluis";
 
 //create your first component
 const Home = () => {
 	const [inputValue, setInputValue] = useState("");
 	const [tareas, setTareas] = useState([]);
+
+	const obtenerTareas = async () => {
+		const response = await fetch(API_URL);
+		if (response.ok) {
+			const body = await response.json();
+			/* setTareas(body); */
+		}
+	};
+
+	useEffect(() => {
+		obtenerTareas();
+	}, []);
+
+	useEffect(() => {
+		console.log(tareas);
+		let objTemp = {};
+		let arrayTemp = [];
+		for (let i = 0; i < tareas.length; i = i + 1) {
+			objTemp = {};
+			objTemp = {
+				label: tareas[i],
+				done: false,
+			};
+			arrayTemp.push(objTemp);
+		}
+		fetch(API_URL, {
+			method: "PUT",
+			body: JSON.stringify(arrayTemp),
+			headers: { "Content-Type": "application/json" },
+		})
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+	}, [tareas]);
 
 	return (
 		<>
@@ -21,15 +55,14 @@ const Home = () => {
 							<input
 								className="mx-2"
 								type="text"
-								onChange={(e) => setInputValue(e.target.value)}
-								onKeyPress={(e) => {
+								/* onChange={(e) => setInputValue(e.target.value)} */
+								onKeyPress={async (e) => {
 									if (e.key == "Enter") {
 										console.log(e.target.value);
-										setTareas((prev) => [
+										await setTareas((prev) => [
 											...prev,
-											inputValue,
+											e.target.value,
 										]);
-										console.log(tareas);
 									}
 								}}
 							/>
@@ -37,20 +70,46 @@ const Home = () => {
 								className="btn btn-danger"
 								onClick={() => {
 									setTareas([]);
+									fetch(API_URL, {
+										method: "DELETE",
+										body: JSON.stringify([]),
+										headers: {
+											"Content-Type": "application/json",
+										},
+									})
+										.then((response) => response.json())
+										.then((data) => console.log(data));
 								}}>
 								eliminar
 							</button>
 						</div>
 					</div>
+					<button
+						className="btn btn-success"
+						onClick={() => {
+							setTareas([]);
+							fetch(API_URL, {
+								method: "POST",
+								body: JSON.stringify([]),
+								headers: {
+									"Content-Type": "application/json",
+								},
+							})
+								.then((response) => response.json())
+								.then((data) => console.log(data));
+						}}>
+						crear usuario
+					</button>
 					<div className="row">
 						<div className="col-2 flex-colunm"></div>
 						<ul className="col-8 list-group list-group-flush">
 							{tareas.map((tarea, index) => {
 								return (
-									<li className=" list-group-item ">
+									<li
+										className=" list-group-item "
+										key={index}>
 										<Tarea
 											nuevaTarea={tarea}
-											key={index}
 											posicion={index}
 											quitarTarea={(removertarea) =>
 												setTareas(
